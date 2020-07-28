131485c9e1b25662287cb33547792f7d559634a2
hodu <hdoo831@naver.com>
2020-07-28 23:05:33 +0900

    소스 코드 간단하게 수정

--- IF/Java14681.java
+++ IF/Java14681.java
@@ -7,21 +7,9 @@ public class Java14681{
         int num1 = sc.nextInt();
         int num2 = sc.nextInt();
 
-        if((-1000 <= num1 && num1 <= 1000) && (-1000 <= num2 && num2 <= 1000)){
-            if(num1 > 0){
-                //첫번째 자리가 양수
-                if(num2 > 0){
-                    System.out.println("1");
-                }else if(num2 < 0){
-                    System.out.println("4");
-                }
-            }else if(num1 < 0){
-                if(num2 > 0){
-                    System.out.println("2");
-                }else if(num2 < 0){
-                    System.out.println("3");
-                }
-            }
-        }
+        if(num1 > 0 && num2 > 0) System.out.println("1");
+        else if(num1 > 0 && num2 < 0) System.out.println("4");
+        else if(num1 < 0 && num2 < 0) System.out.println("3");
+        else if(num1 < 0 && num2 > 0) System.out.println("2");
     }
 }
