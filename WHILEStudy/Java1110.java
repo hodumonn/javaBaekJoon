03afb0bd179d40de4189ad96c21c057979dfa526
hjseo <mijin2197@hanmail.net>
2021-05-31 21:11:24 +0900

    백준 1110문제 (더하기사이클: /와 %를 응용한 풀이)

---
+++ WHILEStudy/Java1110.java
@@ -0,0 +1,38 @@
+package WHILEStudy;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+
+public class Java1110 {
+    
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int cnt = 0;
+        int num1, num2 = 0;
+        int sum = n;
+        
+        
+        if(n < 10){
+            n*=10;
+        }
+        
+        while(true){
+            num1 = sum / 10;
+            num2 = sum % 10;
+            sum = num1 + num2;
+            
+            sum = num2 * 10 + sum % 10;
+            cnt++;
+            
+            if(sum == n)
+                break;
+        }
+        
+        System.out.println(cnt);
+        br.close();
+        
+    }
+}
