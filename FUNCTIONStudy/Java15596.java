1f107ab16d87c8d8fbda3bd054de7439001db604
hjseo <mijin2197@hanmail.net>
2021-06-06 00:51:46 +0900

    백준15596 추가(함수만들기 기초)

---
+++ FUNCTIONStudy/Java15596.java
@@ -0,0 +1,12 @@
+package FUNCTIONStudy;
+
+public class Java15596 {
+    public long sum(int[] a){
+        long sumRst = 0L;
+        for(int b : a){
+            sumRst += b;
+        }
+
+        return sumRst;
+    }
+}
